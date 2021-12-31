66cba1fda23cbdcc203d4d7ed97c603106647737
Fabricio Dorneles <fabriciodorneles@gmail.com>
2021-12-30 21:40:13 -0300

    Feat: Initial Commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        {/* <img src="/images/logo.svg" alt="ig.news" /> */}
+        <nav>
+          <a href="">
+            <img src="/images/question_icon.svg" alt="ig.news" />
+          </a>
+          <a href="">
+            <svg />
+            <img src="/images/right_arrow_icon.svg" alt="ig.news" />
+          </a>
+          <a href="">
+            <img src="/images/at_icon.svg" alt="ig.news" />
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
