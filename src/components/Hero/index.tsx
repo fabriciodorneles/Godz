bdb4406a379bc8a7f7b983a66c532d27c380e749
Fabricio Dorneles <fabriciodorneles@gmail.com>
2022-01-06 20:55:33 -0300

    home/finished

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -27,14 +27,80 @@ export function Hero() {
               <div className={styles.heroTitleContainer}>
                 <p className={styles.textSmall}> BECOME A </p>
                 <div className={styles.buttonsText}>
-                  <div className={styles.buttonG} onFocus={() => setBgState('cernnunos')} onMouseOver={() => setBgState('cernnunos')} onMouseLeave={() => setBgState('')}>
-                    {bgState !== 'cernnunos' && (<img src="/images/G_big.svg" alt="Deus Grego" />) }
+                  <div
+                    className={styles.buttonG}
+                    onFocus={() => setBgState('cernnunos')}
+                    onMouseOver={() => setBgState('cernnunos')}
+                    onMouseLeave={() => setBgState('')}
+                  >
+                    {bgState !== 'cernnunos'
+                      ? (<img src="/images/G_big.svg" alt="Cernnunos" />) : (
+                        <a
+                          style={{ paddingTop: '140px', paddingRight: '70px' }}
+                          href="./what"
+                        >
+                          <img
+                            style={{ width: '40px', height: '40px' }}
+                            src="/images/question_button.svg"
+                            alt="Question Button"
+                            className={styles.hiddenButton}
+                          />
+                        </a>
+                      ) }
                   </div>
-                  <div className={styles.buttonO} onFocus={() => setBgState('cernnunos')} onMouseOver={() => setBgState('grego')} onMouseLeave={() => setBgState('')}>
-                    {bgState !== 'grego' && (<img src="/images/O_big.svg" alt="Deus Grego" />) }
+                  <div style={{ width: '90px' }} />
+                  <div
+                    className={styles.buttonO}
+                    onFocus={() => setBgState('grego')}
+                    onMouseOver={() => setBgState('grego')}
+                    onMouseLeave={() => setBgState('')}
+                  >
+                    {bgState !== 'grego'
+                      ? (
+                        <img
+                          src="/images/O_big.svg"
+                          alt="Deus Grego"
+                        />
+                      ) : (
+                        <a
+                          style={{ paddingTop: '110px', paddingLeft: '80px' }}
+                          href="./how"
+                        >
+                          <img
+                            style={{ width: '40px', height: '40px' }}
+                            src="/images/arrow_button.svg"
+                            alt="Arrow Button"
+                            className={styles.hiddenButton}
+                          />
+                        </a>
+                      ) }
                   </div>
-                  <div className={styles.buttonD} onFocus={() => setBgState('cernnunos')} onMouseOver={() => setBgState('annubis')} onMouseLeave={() => setBgState('')}>
-                    {bgState !== 'annubis' && (<img src="/images/D_big.svg" alt="Deus Grego" />) }
+                  <div style={{ width: '80px' }} />
+                  <div
+                    className={styles.buttonD}
+                    onFocus={() => setBgState('annubis')}
+                    onMouseOver={() => setBgState('annubis')}
+                    onMouseLeave={() => setBgState('')}
+                  >
+                    {bgState !== 'annubis'
+                      ? (
+                        <img
+                          src="/images/D_big.svg"
+                          alt="d_hover"
+                        />
+                      ) : (
+                        <a
+                          style={{ paddingTop: '55px', paddingRight: '50px' }}
+                          href="./who"
+                        >
+                          <img
+                            style={{ width: '40px', height: '40px' }}
+                            src="/images/at_button.svg"
+                            alt="At Button"
+                            className={styles.hiddenButton}
+                          />
+                        </a>
+                      )}
                   </div>
                 </div>
               </div>
