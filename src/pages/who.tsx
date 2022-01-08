a3121e3451b364fd1498a248d2a468a69eb01a6a
Fabricio Dorneles <fabriciodorneles@gmail.com>
2022-01-08 17:29:04 -0300

    finished layout

--- src/pages/who.tsx
+++ src/pages/who.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { Header } from '../components/Header';
 import styles from './who.module.scss';
 
 export default function Home() {
@@ -8,9 +7,15 @@ export default function Home() {
       <Head>
         <title>Godz</title>
       </Head>
-      <Header />
+      <header className={styles.headerContainer}>
+        <nav>
+          <a href="./">
+            {/* <img src="/images/question_icon.svg" alt="ig.news" /> */}
+          </a>
+        </nav>
+      </header>
       <div className={styles.mainContainer}>
-        <div>
+        <div className={styles.setContainer}>
           <div className={styles.containerWho}>
             <div>
               <img src="/images/condz_marcacao.png" alt="Condz" />
@@ -25,17 +30,16 @@ export default function Home() {
                     <a href="https://twitter.com/woconds" target="_blank" rel="noreferrer">@wocondz</a>
                   </div>
                 </div>
-                <div className={styles.twitterWho}>
-                  <img src="/images/twitter_icon.svg" alt="Condz" />
-                </div>
               </div>
               <div className={styles.textWho}>
                 <p>
-                  +10 anos xp em game dev & tech
+                  +10 anos xp em
+                  <br />
+                  game dev & tech
                 </p>
               </div>
               <div className={styles.neoTokyoWho}>
-                <p>NeoTokyo Citizen #1369</p>
+                <a href=" https://opensea.io/assets/0xb668beb1fa440f6cf2da0399f8c28cab993bdd65/1369" target="_blank" rel="noreferrer">NeoTokyo Citizen #1369</a>
               </div>
             </div>
           </div>
@@ -56,27 +60,23 @@ export default function Home() {
                     <a href="https://twitter.com/gustavodesafre1" target="_blank" rel="noreferrer">@gustavodesafre1</a>
                   </div>
                 </div>
-                <div className={styles.twitterWho}>
-                  <img src="/images/twitter_icon.svg" alt="Condz" />
-                </div>
               </div>
               <div className={styles.textWho}>
                 <p>
-                  Engenheiro e MBA em bizdev
+                  Engenheiro e
+                  <br />
+                  MBA em bizdev
                 </p>
               </div>
               <div className={styles.neoTokyoWho}>
-                <p>NeoTokyo Citizen #25</p>
+                <a href="https://opensea.io/assets/0xb668beb1fa440f6cf2da0399f8c28cab993bdd65/2162" target="_blank" rel="noreferrer">NeoTokyo Citizen #25</a>
               </div>
             </div>
           </div>
-          <div className={styles.divider}>
-            <img src="/images/dividerWho.svg" alt="Condz" />
-          </div>
-          <div className={styles.commentContainer}>
-            <div className={styles.millionsContainer}>
-              <p>Milhões em NFTs</p>
-            </div>
+        </div>
+        <div className={styles.commentContainer}>
+          <div className={styles.millionsContainer}>
+            <p>Milhões em NFTs</p>
           </div>
         </div>
       </div>
