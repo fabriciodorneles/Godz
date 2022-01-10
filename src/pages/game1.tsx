ff9383612608f54e7eca71030c7a18df6a7442b8
Fabricio Dorneles <fabriciodorneles@gmail.com>
2022-01-09 21:20:03 -0300

    novo jogo

--- src/pages/game1.tsx
+++ src/pages/game1.tsx
@@ -15,10 +15,7 @@ export default function Home() {
         </nav>
       </header>
       <div className={styles.gameFrame}>
-        <iframe title="TopDown Shooter" src="jogos/index.html" width="600" height="400" scrolling="no" />
-        <div className={styles.instructionsGame}>
-          Use as setas para se mover e mouse para mirar e atirar.
-        </div>
+        <iframe title="TopDown Shooter" src="jogos/index.html" width="335" height="500" scrolling="no" />
       </div>
     </>
   );
