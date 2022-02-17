f384160ead06f269d80b06f1aa8427e2ce1ccd9f
Fabricio Dorneles <fabriciodorneles@gmail.com>
2022-02-16 21:44:29 -0300

    Develop test

---
+++ src/pages/api/getHs.js
@@ -0,0 +1,19 @@
+import faunadb, { query as q } from 'faunadb';
+
+const serverClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
+
+export default async function handler(req, res) {
+  const {highScore, authToken} = JSON.parse(req.body);
+
+  const _response = await serverClient.query(
+    q.Get(
+      q.Select(['instance'], q.KeyFromSecret(authToken)),
+    )
+  )
+  .then((ret) => {
+    return ret
+  })
+  .catch((err) => console.error('Error: %s', err))
+
+  return res.status(200).json({ highScore: _response.data.highScore });
+}
