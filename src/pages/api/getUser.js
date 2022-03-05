6dc3c7db04a78f061589c65c21b1a94aa60f5b6b
fabriciodorneles <fabriciodorneles@gmail.com>
2022-03-05 13:50:37 -0300

    game3-ready

--- src/pages/api/getUser.js
+++ src/pages/api/getUser.js
@@ -16,19 +16,15 @@ const serverClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
 export default async function handler(req, res) {
   const {signed_msg} = JSON.parse(req.body);
   const { address } = await Web3Token.verify(signed_msg);
-  console.log('address ->', address);
   try {
     // Find user
-    console.log('1');
     const user = await serverClient.query(
       Get(
         Match(Index('user_by_public_address'), address)
         )
       )
-    console.log('2', user);
     const accessToken = await createAccessToken(user.ref.id, 3600);
     res.status(200).json({ token: accessToken.secret });
-    console.log('3', accessToken);
     
   } catch (error) {
     // User not found. Ask to Register.
