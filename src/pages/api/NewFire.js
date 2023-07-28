b30db16c4fea61409cda0240c63319e750a4d175
Fabricio Dorneles <fabriciodorneles@gmail.com>
2023-07-28 15:52:55 -0300

    experience

--- src/pages/api/NewFire.js
+++ src/pages/api/NewFire.js
@@ -3,10 +3,11 @@ import faunadb, { query as q } from 'faunadb';
 const serverClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
 
 export default async function handler(req, res) {
-  const {authToken} = JSON.parse(req.body);
+  // const {authToken} = JSON.parse(req.body);
+  const {authToken} = req.body;
 
   return res.status(200).json({
-    token: req,
+    token: authToken,
     planetRun: _response.data.planetRun,
     FireHit: _response.data.FireHit,
     highScore: _response.data.highScore
