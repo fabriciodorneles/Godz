15dce586a160473d5f408e1b38e13797f84d260d
Fabricio Dorneles <fabriciodorneles@gmail.com>
2023-07-28 14:41:20 -0300

    fix endpoint

--- src/pages/api/NewFire.js
+++ src/pages/api/NewFire.js
@@ -3,7 +3,7 @@ import faunadb, { query as q } from 'faunadb';
 const serverClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
 
 export default async function handler(req, res) {
-  const {authToken} = req.body;
+  const {authToken} = JSON.parse(req.body);
 
   const _response = await serverClient.query(
     q.Get(
